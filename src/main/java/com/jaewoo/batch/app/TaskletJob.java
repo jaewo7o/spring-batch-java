d0ec280038afd41970faa8ce391b89e79b1c939a
Jung Jaewoo <jaewo7o@naver.com>
2022-04-10 18:44:22 +0900

    Initial commit

---
+++ src/main/java/com/jaewoo/batch/app/TaskletJob.java
@@ -0,0 +1,50 @@
+package com.jaewoo.batch.app;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@AllArgsConstructor
+@Configuration
+public class TaskletJob {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job taskletJob_batchBuilder() {
+        return jobBuilderFactory.get("taskletJob")
+                .start(taskletJob_step01())
+                .next(taskletJob_step02(null))
+                .build();
+    }
+
+    @Bean
+    public Step taskletJob_step01() {
+        return stepBuilderFactory.get("taskletJob_step01")
+                .tasklet((x, y) -> {
+                    log.info("tasklet log Job -> Step");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    @JobScope
+    public Step taskletJob_step02(@Value("#{jobParameters[date]}") String date) {
+        return stepBuilderFactory.get("taskletJob_step02")
+                .tasklet((x, y) -> {
+                    log.info("tasklet log Step01 -> Step02, date : " + date);
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
---
+++ src/test/java/com/jaewoo/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewoo.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
