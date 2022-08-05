b906ff782f716c82e8d749fa5abb7592b7368176
Jung Jaewoo <jaewo7o@naver.com>
2022-08-05 16:43:25 +0900

    batch test case 추가

---
+++ src/main/java/com/jaewoo/batch/app/basic/HelloWorldJob.java
@@ -0,0 +1,35 @@
+package com.jaewoo.batch.app.basic;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class HelloWorldJob {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob_batchBuilder() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(helloWorldJob_step01())
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldJob_step01() {
+        return stepBuilderFactory.get("helloWorldJob_step01")
+                .tasklet((x, y) -> {
+                    log.info("tasklet log Job -> Step");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
---
+++ src/test/java/com/jaewoo/batch/app/basic/HelloWorldJobTest.java
@@ -0,0 +1,47 @@
+package com.jaewoo.batch.app.basic;
+
+import com.jaewoo.batch.core.test.BatchTestConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBatchTest
+@SpringBootTest(classes = {HelloWorldJob.class, BatchTestConfig.class}) // (2)
+class HelloWorldJobTest {
+
+    @Autowired
+    protected JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    @DisplayName("Hello World Test")
+    void helloWorldJob_batchBuilder() throws Exception {
+        // given
+        JobParameters jobParameters = jobLauncherTestUtils.getUniqueJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assertions.assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
+
+    @Test
+    @DisplayName("Hello World Test - Tasklet Step")
+    void helloWorldJob_step() {
+        // given
+        String stepName = "helloWorldJob_step01";
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep(stepName);
+
+        // then
+        Assertions.assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
+}
